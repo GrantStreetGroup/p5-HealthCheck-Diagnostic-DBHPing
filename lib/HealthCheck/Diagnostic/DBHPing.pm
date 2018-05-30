fe5664309f638c67a0f83b4c280587c74ee46d79
Andrew Fresh <andrew.fresh@grantstreet.com>
2018-05-30 13:39:16 -0400

    Add basic boilerplate for new module and tests

---
+++ lib/HealthCheck/Diagnostic/DBHPing.pm
@@ -0,0 +1,57 @@
+package HealthCheck::Diagnostic::DBHPing;
+
+# ABSTRACT: Ping your database to check its health.
+# VERSION
+
+use 5.010;
+use strict;
+use warnings;
+use parent 'HealthCheck::Diagnostic';
+
+sub new {
+    my ($class, @params) = @_;
+
+    # Allow either a hashref or even-sized list of params
+    my %params = @params == 1 && ( ref $params[0] || '' ) eq 'HASH'
+        ? %{ $params[0] } : @params;
+
+    return $class->SUPER::new(
+        label => 'dbh_ping',
+        %params
+    );
+}
+
+sub run {
+    my ($self) = @_;
+    return { status => 'OK' };
+}
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+    my $health_check = HealthCheck->new( checks => [
+        HealthCheck::Diagnostic::DBHPing->new( dbh => $dbh )
+    ] );
+
+    my $result = $health_check->check;
+
+=head1 DESCRIPTION
+
+Calls C<< dbh->ping >> and checks the truthiness of the result to
+determine if the database connection is valid and available.
+
+=head1 ATTRIBUTES
+
+=head2 dbh
+
+A L<DBI database handle object|DBI/DBI-DATABSE-HANDLE-OBJECTS>.
+
+=head1 DEPENDENCIES
+
+L<HealthCheck::Diagnostic>
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+None
---
+++ t/HealthCheck-Diagnostic-DBHPing.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use Test::More;
+
+use HealthCheck::Diagnostic::DBHPing;
+
+pass "Loaded OK";
+
+done_testing;
